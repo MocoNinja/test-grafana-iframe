0e70294bb89b3239a4b3a651e45428c413516563
MocoNinja <javiergonzaleztello@gmail.com>
2021-09-01 11:11:57 +0200

    Disminuyendo un poco el nivel de kk
    Signed-off-by: MocoNinja <javiergonzaleztello@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { useState } from 'react';
 import DateTimePicker from 'react-datetime-picker';
-
-import {dateToTimestamp, timestampToDate, timestampToIsoDate } from './utils';
-
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
 
   const [datetime, setDatetime] = useState({
     "from-ts": 1629962313743,
     "to-ts": 1629967233302,
-    "from-date": timestampToIsoDate(1629962313743) ,
-    "to-date": timestampToIsoDate(1629967233302),
+    "from-date": new Date(1629962313743),
+    "to-date": new Date(1629967233302),
   });
-
-
-
-  const toggleSearch = () => {
-    const dateToSet = (datetime["from-ts"] === 1629957394185)
-    ? {"from-ts": 1629962313743, "to-ts": 1629967233302}
-    : {"from-ts": 1629957394185, "to-ts": 1630015199000}
-    setDatetime(dateToSet)
-  }
 
   const getIframe = (search) => {
     const url = `http://raspberrypi.local:3000/grafana/d-solo/xfpJB9FGz/1-node-exporter-for-prometheus-dashboard-en-v20201010?orgId=1&from=${search["from-ts"]}&to=${search["to-ts"]}&var-origin_prometheus=&var-job=node_exporter&var-hostname=All&var-node=raspberrypi4:9100&var-device=All&var-interval=2m&var-maxmount=%2Fmnt%2FEstraible&var-show_hostname=raspberrypi4&var-total=1&panelId=7`;
@@ -29,54 +18,59 @@ function App() {
   }
 
   const changedatefrom = (data) => {
-    if (data !== null)  {
-    setDatetime({
-      "to-ts": datetime['to-ts'],
-      "to-date": datetime["to-date"],
-      "from-ts": dateToTimestamp(data),
-      "from-date": data 
-    })
-  }
+    if (data !== null) {
+      setDatetime({
+        "to-ts": datetime['to-ts'],
+        "to-date": datetime["to-date"],
+        "from-ts": data.getTime(),
+        "from-date": data
+      })
+    }
   }
 
   const changedateto = (data) => {
-    if (data !== null)  {
-    setDatetime({
-      "from-ts": datetime['from-ts'],
-      "from-date": datetime["from-date"],
-      "to-ts": dateToTimestamp(data),
-      "to-date": data 
-    })
-  }
+    if (data !== null) {
+      setDatetime({
+        "from-ts": datetime['from-ts'],
+        "from-date": datetime["from-date"],
+        "to-ts": data.getTime(),
+        "to-date": data
+      })
+    }
   }
 
-  return (
-    <div className="App">
-      <p>{JSON.stringify(datetime)}</p>
-        {
-          /*
-          <iframe src="http://raspberrypi.local:3000/grafana/?orgId=1&from=1630399657148&to=1630421257148&panelId=7" width="1920" height="1080"/>
-          */
-        }
-        {/*
-        <iframe src="http://raspberrypi.local:3000/grafana/d-solo/xfpJB9FGz/1-node-exporter-for-prometheus-dashboard-en-v20201010?orgId=1&from=1630378301635&to=1630421501635&panelId=7" width="1920" height="1080" frameborder="0"></iframe>
-      */}
-      {getIframe(datetime)}
-      <button onClick={toggleSearch} name="TOGGLE" value="TOGGLE">TOGGLE</button>
-      <DateTimePicker
-        onChange={changedatefrom}
-        disableClock={true}
-        clearIcon={null}
-        format="y-MM-dd HH:mm:ss"
-        value={datetime['from-date']}
+  const renderDatePickers = () =>
+    <>
+      <div className="col-2 mx-auto">
+        <DateTimePicker
+          onChange={changedatefrom}
+          disableClock={true}
+          clearIcon={null}
+          format="y-MM-dd HH:mm:ss"
+          value={datetime['from-date']}
         />
-      <DateTimePicker
-        onChange={changedateto}
-        disableClock={true}
-        clearIcon={null}
-        format="y-MM-dd HH:mm:ss"
-        value={datetime['to-date']}
-      />
+      </div>
+      <div className="col-3 mx-auto">
+        <DateTimePicker
+          onChange={changedateto}
+          disableClock={true}
+          clearIcon={null}
+          format="y-MM-dd HH:mm:ss"
+          value={datetime['to-date']}
+        />
+      </div>
+    </>
+
+  return (
+    <div className="container m-1">
+      <header className="text-center">
+        <h3>Gráfica de CPU </h3>
+      </header>
+      <div className="row p-3">
+        {renderDatePickers()}
+      </div>
+      <div className="row">
+        {getIframe(datetime)}</div>
     </div>
   );
 }
